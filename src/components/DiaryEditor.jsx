cebfb28b3a5705bca2a39072f5d2705dc30cb1d6
hwayoung <hyriver37@gamil.com>
2023-09-21 07:38:42 +0900

    [feat] isEdit이 true일때, onEdit 함수 호출하여 페이지 렌더링

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -1,6 +1,6 @@
 // 새일기쓰기 페이지와 수정하기 페이지의 형태가 동일하므로 컴포넌트를 만들어서 재사용 가능하도록
 import { useNavigate } from "react-router-dom";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { DiaryDispatchContext } from "./../App.js";
 
 import MyHeader from "./MyHeader";
@@ -44,13 +44,13 @@ const getStringDate = (date) => {
   return date.toISOString().slice(0, 10);
 };
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
   const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const { onCreate } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
 
   // <emotionItem>을 클릭하면 onClick prop으로 emotion_id를 받으면서
   // 현재 어떤 emotion이 선택됐는지 알 수 있다
@@ -60,20 +60,39 @@ const DiaryEditor = () => {
 
   const navigate = useNavigate();
 
-  //
   const handleSubmit = () => {
     if (content.length < 1) {
       contentRef.current.focus();
       return; //더 이상 진행 못하도록
     }
-    onCreate(date, content, emotion);
+    if (
+      window.confirm(
+        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion);
+      } else {
+        onEdit(originData.id, date, content, emotion);
+      }
+    }
+
     navigate("/", { replace: true }); // 홈으로 돌아가게 ✅replace: true 작성 완료 후 뒤로가기 못하도록
   };
+
+  useEffect(() => {
+    if (isEdit) {
+      //idEdit가 있을때 이 페이지가 렌더링 됨
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
 
   return (
     <div className="DiaryEditor">
       <MyHeader
-        headText={"새 일기쓰기"}
+        headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
         }
