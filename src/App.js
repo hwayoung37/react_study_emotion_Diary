56b679ab33140a03187f612e53259d842081ed6d
hwayoung <hyriver37@gamil.com>
2023-10-13 01:38:49 +0900

    [Fix] 일기 삭제 후 홈에서 화면 출력 안되는 에러 수정

--- src/App.js
+++ src/App.js
@@ -53,9 +53,12 @@ function App() {
       const diaryList = JSON.parse(localData).sort(
         (a, b) => parseInt(b.id) - parseInt(a.id)
       );
-      dataId.current = parseInt(diaryList[0].id + 1);
 
-      dispatch({ type: "INIT", data: diaryList });
+      if (diaryList.length >= 1) {
+        //빈배열이 아닐때
+        dataId.current = parseInt(diaryList[0].id + 1);
+        dispatch({ type: "INIT", data: diaryList });
+      }
     }
   }, []);
 
